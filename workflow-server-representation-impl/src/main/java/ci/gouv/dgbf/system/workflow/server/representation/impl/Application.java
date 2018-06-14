cff7988b1007fde513dfbfcddf2e3f58dce0129a
Christian Yao Komenan <Christian@CYK-HP-LAPTOP>
2018-06-14 00:27:31 +0000

    First commit

---
+++ workflow-server-representation-impl/src/main/java/ci/gouv/dgbf/system/workflow/server/representation/impl/Application.java
@@ -0,0 +1,15 @@
+package ci.gouv.dgbf.system.workflow.server.representation.impl;
+
+@javax.ws.rs.ApplicationPath("/")
+@javax.ws.rs.Path("/")
+@javax.enterprise.context.ApplicationScoped
+public class Application extends javax.ws.rs.core.Application {
+
+	@javax.ws.rs.GET
+	@javax.ws.rs.Path("/")
+	@javax.ws.rs.Produces(javax.ws.rs.core.MediaType.TEXT_PLAIN)
+	public String getRootResource() {
+		return "Hi from workflow system server. Time is "+new java.util.Date();
+	}
+	
+}
